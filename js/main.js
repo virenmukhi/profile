e9c1320d1ab7647f0d333e82ca7c6cb3ac1e986d
Viren Mukhi <viren@mgglobe.com>
2019-07-09 04:38:47 +0800

    version 2 first draft

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// contact form
 
 (function ($) {
     "use strict";
@@ -13,11 +14,11 @@
             else {
                 $(this).removeClass('has-val');
             }
-        })    
+        })
     })
-            
-  
-    
+
+
+
     /*==================================================================
     [ Validate ]*/
     var name = $('.validate-input input[name="name"]');
@@ -65,7 +66,56 @@
 
         $(thisAlert).removeClass('alert-validate');
     }
-    
-    
+
+
 
 })(jQuery);
+
+
+
+
+// Page Scroll Feature
+
+// Select all links with hashes
+    $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+  // flip card
+
+  function flip() {
+  $('.flipcard').toggleClass('flipped');
+}
+
